e62a79b434750a9206e16e290042ef5ebc6e5e85
Justin Ceronio <>
2022-12-14 11:14:27 +0100

    Add Log4J

--- server/src/main/java/ch/antihack/server/ServerApplication.java
+++ server/src/main/java/ch/antihack/server/ServerApplication.java
@@ -1,5 +1,7 @@
 package ch.antihack.server;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ServerApplication {
 
+	private static final Logger logger = LogManager.getLogger(ServerApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
 	@GetMapping("/")
 	public StudentList greet() {
+		logger.info("Greeting someone");
 		return new StudentList(new String[] {"Inschallah", "Maschallah"});
 	}
 
