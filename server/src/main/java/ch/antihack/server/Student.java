c1a71a2959ad44200797a2ace734a5772f36e650
Adrian Spycher <asp121206@iet-gibb.ch>
2022-12-14 11:38:58 +0100

    Student-Domain Class complete

--- server/src/main/java/ch/antihack/server/Student.java
+++ server/src/main/java/ch/antihack/server/Student.java
@@ -5,12 +5,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.sql.Blob;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 public class Student {
     private int id;
+    private Blob profilePicture;
+    private String firstName;
     private String name;
-    private String classroom;
+    private String address;
+    private Integer zipCode;
+    private String email;
 }
+
