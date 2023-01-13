79511ccad7aefc471d97c258d0b9fb6ac586a38a
Justin Ceronio <justin.ceronio@outlook.com>
2023-01-13 22:07:43 +0100

    Add property for jwt expiration date and prevent user registering with existing email

--- server/src/main/java/ch/antihack/server/controller/AuthenticationController.java
+++ server/src/main/java/ch/antihack/server/controller/AuthenticationController.java
@@ -1,10 +1,13 @@
 package ch.antihack.server.controller;
 
+import ch.antihack.server.ServerApplication;
 import ch.antihack.server.model.AuthenticationRequest;
 import ch.antihack.server.model.AuthenticationResponse;
 import ch.antihack.server.model.RegisterRequest;
 import ch.antihack.server.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,11 +21,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
 
+    private static final Logger logger = LogManager.getLogger(ServerApplication.class);
+
     private final AuthenticationService service;
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
-        System.out.println("This funny guy wants to register");
+        logger.info("User is trying to register with email " + request.getEmail());
         return ResponseEntity.ok(service.register(request));
     }
     @PostMapping("/authenticate")
