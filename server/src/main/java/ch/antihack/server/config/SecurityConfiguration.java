35abb5bba93efd5a8b0618e4d242139dc8bf82b4
Justin Ceronio <>
2023-01-13 18:48:55 +0100

    Enable https

--- server/src/main/java/ch/antihack/server/config/SecurityConfiguration.java
+++ server/src/main/java/ch/antihack/server/config/SecurityConfiguration.java
@@ -1,7 +1,6 @@
 package ch.antihack.server.config;
 
 
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
