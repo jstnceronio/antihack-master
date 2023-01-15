d91997fed17fa80378ed33fdacf137aa6e837d95
ynnbnn <yann.flueckiger@gmx.ch>
2023-01-15 21:15:08 +0100

    cleaning up HTML

--- antihack-client/src/app/register/register.component.ts
+++ antihack-client/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Component} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
 
