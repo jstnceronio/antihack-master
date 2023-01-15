a371b61d238c29e0826d2e21eeba6a37e019781c
Adrian Spycher <asp121206@iet-gibb.ch>
2023-01-15 20:48:17 +0100

    refreshToken generated

--- antihack-server/src/main/java/ch/antihack/server/config/JwtAuthenticationFilter.java
+++ antihack-server/src/main/java/ch/antihack/server/config/JwtAuthenticationFilter.java
@@ -32,6 +32,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     ) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
+        final String jwtRefeshToken;
         final String userEmail;
         if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
